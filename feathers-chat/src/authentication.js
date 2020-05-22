ccf6a6774c10073207be52c416588e18c7924e65
Ankita Sahoo <2ankitasahoo13@gmail.com>
2020-05-22 22:35:27 +0530

    added the codes from the documentation

---
+++ feathers-chat/src/authentication.js
@@ -0,0 +1,27 @@
+const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
+const { LocalStrategy } = require('@feathersjs/authentication-local');
+const { expressOauth, OAuthStrategy } = require('@feathersjs/authentication-oauth');
+
+class GithubStrategy extends OAuthStrategy {
+  async getEntityData(profile) {
+    const baseData = await super.getEntityData(profile);
+
+    return {
+      ...baseData,
+      name: profile.login,
+      avatar: profile.avatar_url,
+      email: profile.email
+    };
+  }
+}
+
+module.exports = app => {
+  const authentication = new AuthenticationService(app);
+
+  authentication.register('jwt', new JWTStrategy());
+  authentication.register('local', new LocalStrategy());
+  authentication.register('github', new GithubStrategy());
+  
+  app.use('/authentication', authentication);
+  app.configure(expressOauth());
+};
